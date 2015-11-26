68ded501894d182909e840e636486142c9abf8bb
jon <j7caiman@users.noreply.github.com>
2015-11-26 16:42:05 -0500

    file write example

---
+++ file_io_examples/writeFile/src/example.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var fileContents = "When they kick at your front door" +
+	"\nHow you gonna come?" +
+	"\nWith your hands on your head" +
+	"\nOr on the trigger of your gun" +
+	"\nWhen the law break in" +
+	"\nHow you gonna go?" +
+	"\nShot down on the pavement" +
+	"\nOr waiting on death row";
+
+fs.writeFile("sample.txt", fileContents, function(err) {
+	if(err) {
+		throw err;
+	}
+});
