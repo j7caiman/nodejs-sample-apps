6d7044aa60a11768a4c8be2c24220f79de4e9178
jon <j7caiman@users.noreply.github.com>
2016-02-08 18:55:30 +0100

    session login example

---
+++ session_examples/login/src/app.js
@@ -0,0 +1,85 @@
+var Sequelize = require('sequelize');
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var sequelize = new Sequelize('class_example', 'jon', null, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: false
+	}
+});
+
+var User = sequelize.define('user', {
+	name: Sequelize.STRING,
+	email: Sequelize.STRING,
+	password: Sequelize.STRING
+});
+
+var app = express();
+
+app.use(session({
+	secret: 'oh wow very secret much security',
+	resave: true,
+	saveUninitialized: false
+}));
+
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+
+app.get('/', function (request, response) {
+	response.render('index', {
+		message: request.query.message,
+		user: request.session.user
+	});
+});
+
+app.get('/profile', function (request, response) {
+	var user = request.session.user;
+	if (user === undefined) {
+		response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+	} else {
+		response.render('profile', {
+			user: user
+		});
+	}
+});
+
+app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
+	User.findOne({
+		email: request.body.email
+	}).then(function (user) {
+		if (request.body.password === user.password) {
+			request.session.user = user;
+			response.redirect('/profile');
+		} else {
+			response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+		}
+	}, function (error) {
+		response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+	});
+});
+
+app.get('/logout', function (request, response) {
+	request.session.destroy(function(error) {
+		if(error) {
+			throw error;
+		}
+		response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+	})
+});
+
+sequelize.sync({force: true}).then(function () {
+	User.create({
+		name: "stabbins",
+		email: "yes@no",
+		password: "not_password"
+	});
+}, function () {
+	console.log('sync failed');
+}).then(function () {
+	var server = app.listen(3000, function () {
+		console.log('Example app listening on port: ' + server.address().port);
+	});
+});
