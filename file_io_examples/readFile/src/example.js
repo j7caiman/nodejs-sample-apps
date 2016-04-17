c6c2c831bd14f2c2692b4a5ccd7fc637fd3286a9
jon <j7caiman@users.noreply.github.com>
2016-04-17 01:38:01 -0400

    update file io example

--- file_io_examples/readFile/src/example.js
+++ file_io_examples/readFile/src/example.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
-fs.readFile('./sample.txt', function (err, data) {
+fs.readFile('./sample.txt', 'utf8', function (err, data) {
 	if (err) {
+		console.log("looks like an error occurred: " + err);
 		throw err;
 	}
 
-	var text = data.toString();
-	console.log(text);
+	console.log("file read successfully:");
+	console.log(data);
 });
