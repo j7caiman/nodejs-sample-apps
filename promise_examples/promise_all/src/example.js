8d2858f4192f2fc7a67694f41467970092f3588e
jon <j7caiman@users.noreply.github.com>
2016-04-17 01:40:18 -0400

    update comment

--- promise_examples/promise_all/src/example.js
+++ promise_examples/promise_all/src/example.js
@@ -1,8 +1,7 @@
 /*
  Read in a file, named "people1.json", which contains a list of people.
  Read in another file, named "people2.json", which contains another list of people.
- Get them here: people2.jsonView in a new window, people.jsonView in a new window
- After both of these files have been read, write a new file called "peopleComplete.txt" which contains a sorted list of all the people from the first two files.
+ After both of these files have been read, write a new file called "peopleComplete.json" which contains a sorted list of all the people from the first two files.
 
  BONUS:
  Read in N files from a people directory, and merge all of the files in the directory into a peopleComplete.json file
