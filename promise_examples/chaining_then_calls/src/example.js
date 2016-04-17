5ebf36dfccb2c041b43035331f46c2e07e4d100e
jon <j7caiman@users.noreply.github.com>
2016-04-17 01:42:12 -0400

    make example a little easier

--- promise_examples/chaining_then_calls/src/example.js
+++ promise_examples/chaining_then_calls/src/example.js
@@ -4,7 +4,11 @@ var Promise = require('promise');
 var readFile = Promise.denodeify(fs.readFile);
 
 readFile('./sample.json', 'utf8')
-	.then(JSON.parse, console.log)
+	.then(function(data) {
+		return JSON.parse(data);
+	}, function(err) {
+		console.log('there was an error: ' + err);
+	})
 	.then(function (parsedResult) {
 		console.log("printing out a specific value: " + parsedResult.sample_key);
 	}
